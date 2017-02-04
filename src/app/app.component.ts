bbbcd2aef4431baa5d74cf163e0541c8558505f2
KEPE <kepe@nykredit.dk>
2017-02-04 10:20:52 +0100

    sidenav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface MenuItem {
+    icon: string;
+    title: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    readonly menuItems: Array<MenuItem> = [
+        {icon: 'account_balance', title: 'Account'},
+        {icon: 'monetization_on', title: 'Transactions'},
+        {icon: 'directions_bike', title: 'other content'},
+    ];
 }
